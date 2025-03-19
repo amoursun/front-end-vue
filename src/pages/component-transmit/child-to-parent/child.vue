901c73f2084f89253a73d5afe88e37cd1965fd7a
ganyanlin <ganyanlin@baidu.com>
2025-03-19 16:29:47 +0800

    feat: MinusSquareOutlined

---
+++ src/pages/component-transmit/child-to-parent/child.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="child">
+		<input type="text" v-model="msg" />
+		<button @click="onHandler">发送信息给父级</button>
+	</div>
+</template>
+<script setup lang="ts" name="Child">
+	import {defineEmits, ref} from 'vue';
+	const msg = ref('我是子组件');
+	const emit = defineEmits(['handler']);
+	function onHandler() {
+		emit('handler', msg.value);
+	}
+</script>
+
+<style scoped lang="scss">
+	.child{
+		margin-top: 20px;
+		border: 1px solid #CCC;
+		padding: 10px;
+	}
+</style>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,10 @@ declare namespace NodeJS {
     type Timer = any;
     type Timeout = any;
 }
+declare module '*.vue' {
+    import Vue from 'vue';
+    export default Vue;
+}
 declare module '*.tsx' {
     import {DefineComponent} from 'vue';
     const component: DefineComponent<{}, {}, any>
