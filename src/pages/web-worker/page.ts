3a11276a2370d3be446ae76a160049dda667064a
ganyanlin <ganyanlin@baidu.com>
2025-03-25 21:05:25 +0800

    [BPIT-HCM-93464] vxe-table

--- src/pages/web-worker/page.ts
+++ src/pages/web-worker/page.ts
@@ -4,5 +4,6 @@ export const page = {
 };
 /**
  * 一文彻底了解Web Worker，十万条数据都是弟弟: https://juejin.cn/post/7137728629986820126
+ * vxe-table: https://vxetable.cn/v4/#/demo/product
  */
 export default page;
