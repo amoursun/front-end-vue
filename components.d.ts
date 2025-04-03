2120306c192da75846e846e8d94be9cfa1b73f8d
ganyanlin <ganyanlin@baidu.com>
2025-04-03 18:54:22 +0800

    [BPIT-HCM-92156] 按钮逻辑控制

--- components.d.ts
+++ components.d.ts
@@ -59,6 +59,7 @@ declare module 'vue' {
     ASteps: typeof import('ant-design-vue/es')['Steps']
     ASubMenu: typeof import('ant-design-vue/es')['SubMenu']
     ASwitch: typeof import('ant-design-vue/es')['Switch']
+    ATable: typeof import('ant-design-vue/es')['Table']
     ATabPane: typeof import('ant-design-vue/es')['TabPane']
     ATabs: typeof import('ant-design-vue/es')['Tabs']
     ATag: typeof import('ant-design-vue/es')['Tag']
